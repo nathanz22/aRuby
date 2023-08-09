8f566b5223b8a70c9e792ddd7b2b39e8ce72006b
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-09 20:27:15 -0300

    Atualizado ex009

--- exercicios/ex009.rb
+++ exercicios/ex009.rb
@@ -2,12 +2,22 @@
 
 puts ' Exercício 009 '.center(85, '=-')
 
-print 'Qual o valor a ser convertido? : '
-v = Float(gets.chomp)
-d = v * 4.8997
-e = v * 5.3674
+loop do
+  print 'Qual o valor a ser convertido? : R$'
+  v = Float(gets.chomp)
+rescue ArgumentError
+  puts 'ERRO | Valor inválido!'
+rescue Interrupt
+  puts "\nINTERRUPÇÃO | O usuário preferiu interromper"
+  puts ''.center(85, '=-')
+  exit
+else
+  d = v * 4.8997
+  e = v * 5.3674
 
-puts " - Valor: R$#{format('%.2f', v)}"
-puts " - Em dólar: $ #{format('%.2f', d)}\n - Em euro: € #{format('%.2f', e)}"
+  puts " - Em real: R$#{format('%.2f', v)}"
+  puts " - Em dólar: $ #{format('%.2f', d)}\n - Em euro: € #{format('%.2f', e)}"
+  break
+end
 
 puts ''.center(85, '=-')
