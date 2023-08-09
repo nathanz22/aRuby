ba55f14743dac66bfea054089c833e45b4d6ebe9
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-09 20:13:01 -0300

    Atualizado ex006

--- exercicios/ex006.rb
+++ exercicios/ex006.rb
@@ -2,10 +2,23 @@
 
 puts ' Exercício 006 '.center(85, '=-')
 
-print 'Digite a primeira nota: '
-n1 = Float(gets.chomp)
-print 'Digite a segunda nota: '
-n2 = Float(gets.chomp)
+def input(msg)
+  loop do
+    print msg
+    n = Float(gets.chomp)
+  rescue ArgumentError
+    puts 'ERRO | Valor inválido!'
+  rescue Interrupt
+    puts "\nINTERRUPÇÃO | O usuário preferiu interromper"
+    puts ''.center(85, '=-')
+    exit
+  else
+    return n
+  end
+end
+
+n1 = input('Digite a primeira nota: ')
+n2 = input('Digite a segunda nota: ')
 
 puts "\nNota 1: #{n1} | Nota 2: #{n2}\nMédia: #{(n1 + n2) / 2}"
 
