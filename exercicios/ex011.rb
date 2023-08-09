1ed6adf675b460d364928926ca49a50e63fd8291
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-09 20:59:16 -0300

    Atualizado o ex011

--- exercicios/ex011.rb
+++ exercicios/ex011.rb
@@ -2,26 +2,56 @@
 
 puts ' Exercício 011 '.center(85, '=-')
 
-print 'Aumento ou desconto? [A/D]: '
-choice = String(gets.chomp).strip.upcase
+def input(msg, i_or_f)
+  loop do
+    print msg
+    case i_or_f.upcase
+    when 'I'
+      v = Integer(gets.chomp)
+    when 'F'
+      v = Float(gets.chomp)
+    end
+  rescue ArgumentError
+    puts 'ERRO | Valor inválido!'
+  rescue Interrupt
+    puts "\nINTERRUPÇÃO | O usuário preferiu interromper"
+    puts ''.center(85, '=-')
+    exit
+  else
+    return v
+  end
+end
+
+def a_or_d
+  loop do
+    print 'Aumento ou desconto? [A/D]: '
+    choice = String(gets.chomp).strip.upcase
+    if choice != 'A' && choice != 'D'
+      puts 'ERRO | Opção inválida! Digite "A" ou "D"'
+      next
+    end
+  rescue Interrupt
+    puts "\nINTERRUPÇÃO | O usuário preferiu interromper"
+    puts ''.center(85, '=-')
+    exit
+  else
+    return choice
+  end
+end
+
+choice = a_or_d
 
 case choice
 when 'A'
-  print 'Valor: R$'
-  v = Float(gets.chomp)
-
-  print 'Valor para aumento (%): '
-  va = Integer(gets.chomp)
+  v = input('Valor: R$', 'f')
+  va = input('Valor para aumento (%): ', 'I')
 
   a1 = v * va / 100
   a2 = v + a1
   puts "\n - Valor: R$#{format('%.2f', v)} | Aumento: #{va}%\n - O valor já com o aumento é R$#{format('%.2f', a2)}"
 when 'D'
-  print 'Valor: R$'
-  v = Float(gets.chomp)
-
-  print 'Valor para desconto (%): '
-  vd = Integer(gets.chomp)
+  v = input('Valor: R$', 'F')
+  vd = input('Valor para desconto (%): ', 'I')
 
   d1 = v * vd / 100
   d2 = v - d1
