085b7fc0b61463782c53fdb7e9e353017697f27e
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-09 20:18:12 -0300

    Atualizado ex007

--- exercicios/ex007.rb
+++ exercicios/ex007.rb
@@ -2,9 +2,17 @@
 
 puts ' Exercício 007 '.center(85, '=-')
 
-print 'Digite um valor em METRO: '
-m = Float(gets.chomp)
-
-puts "\nMetro: #{m} | Centímetro: #{m * 100} | Milímetro: #{m * 1000}"
+loop do
+  print 'Digite um valor em METRO: '
+  m = Float(gets.chomp)
+rescue ArgumentError
+  puts 'ERRO | Valor inválido!'
+rescue Interrupt
+  puts "\nINTERRUPÇÃO | O usuário preferiu interromper"
+  puts ''.center(85, '=-')
+  exit
+else
+  puts "\nMetro: #{m} | Centímetro: #{m * 100} | Milímetro: #{m * 1000}"
+end
 
 puts ''.center(85, '=-')
