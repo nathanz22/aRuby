e03c15f7ecb37575850d9fb6de312f9e65850601
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-14 17:36:22 -0300

    Atualizado ex014

--- exercicios/ex014.rb
+++ exercicios/ex014.rb
@@ -1,73 +1,78 @@
-puts ' Exercício 014 '.center(85, '=-')
+# ex014: Arrendondar um número
 
-# ----- Função que captura a entrada do usuário para valores float ----- #
-def input(msg)
+# Função que captura a entrada do usuário para valores float
+def input_float(msg)
   loop do
     print msg # Mostra a mensagem passada como argumento
-    num = Float(gets.chomp) # Pede a entrada do usuário
+    return Float(gets.chomp) # Pede a entrada do usuário e a retorna
   rescue ArgumentError # Caso o usuário digite um valor inválido (não numérico)
-    puts "\e[31mERRO | Valor deve ser um número de real (ponto flutuante)\e[0m"
+    puts "\e[31mERRO | Valor deve ser um número. Tente novamente\e[0m"
   rescue Interrupt # Caso o usuário interrompa o script
     puts "\n\e[33mINTERRUPÇÃO | O usuário preferiu interromper\e[0m"
-    puts ''.center(85, '=-')
+    g_linha
     exit
-  else # Caso o valor passado seja válido
-    return num # Retorna o valor passado pelo usuário
   end
 end
 
-def linha(num)
-  # Mostra uma linha
-  puts '-' * num
-end
-
-# ----- Função que pergunta ao usuário se quer ou não encerrar o script ----- #
-def encerramento
-  loop do
-    linha(30)
-    print 'Deseja encerrar? [S/N]: '
-    choice = String(gets.chomp).strip.upcase
-    linha(30)
-  rescue Interrupt # Caso o usuário interrompa o script
-    puts "\n\e[33mINTERRUPÇÃO | O usuário preferiu interromper\e[0m"
-    puts ''.center(85, '=-')
-    exit
-  else
-    case choice
-    when 'S' # Retorna falso caso o usuário queira encerrar
-      puts "\e[33mENCERRAMENTO | Até mais!\e[0m"
-      return false
-    when 'N' # Retorna verdadeiro caso o usuário não queira encerrar
-      # --- Limpa o terminal depois de veriricar o sistema operacional do usuário --- #
-      if Gem.win_platform?
-        system 'cls' # Limpa o terminal (Windows)
-      else
-        system 'clear' # Limpa o terminal (Outros sistemas operacionais)
-      end
-      puts ''.center(85, '=-')
-      return true
-    else
-      puts "\e[31mERRO | Opção inválida! Digite \"S\" para SIM ou \"N\" para NÃO\e[0m"
-    end
-  end
-end
-
-# ----- Arredonda um número ----- #
-def arredondador(num)
+# Função para arredondar um número
+def arredondar(num)
   num.round
 end
 
-# ----- Código Principal ----- #
-c = true # Condição para o while
-while c
-  num = input('Digite o número a ser arredondado: ')
-  num_a = arredondador(num)
-
+# Função para mostrar o resultado
+def mostrar_resultado(num, num_arredondado)
   linha(50)
-  puts " - Número: \e[32m#{num}\e[0m | Número arredondado: \e[33m#{num_a}\e[0m".center(55, ' ')
-
-  # --- Encerrar o programa --- #
-  c = encerramento
+  puts " - Número: \e[32m#{num}\e[0m | Número arredondado: \e[33m#{num_arredondado}\e[0m".center(55, ' ')
 end
 
-puts ''.center(85, '=-')
+# Função que mostra uma linha de tamanho personalizado
+def linha(num)
+  puts '-' * num
+end
+
+# Função que mostra uma grande linha padronizada
+def g_linha
+  puts ''.center(85, '=-')
+end
+
+# Função para limpar o terminal
+def limpar_terminal
+  system('clear') || system('cls') || puts("\e[H\e[2J")
+end
+
+# Função que pergunta ao usuário se quer ou não encerrar o script
+def encerramento
+  loop do
+    linha(30)
+    print 'Deseja continuar? [S/N]: '
+    choice = String(gets.chomp).strip.upcase
+    linha(30)
+    case choice
+    when 'S', 'SIM' # Caso o usuário queira continuar
+      limpar_terminal
+      g_linha
+      return true
+    when 'N', 'NAO', 'NÃO' # Caso o usuário NÃO queira continuar
+      puts "\e[33mENCERRAMENTO | Até mais!\e[0m"
+      return false
+    else
+      puts "\e[31mERRO | Opção inválida! Digite \"S\" para CONTINUAR ou \"N\" para ENCERRAR\e[0m"
+    end
+  rescue Interrupt # Caso o usuário interrompa o script
+    puts "\n\e[33mINTERRUPÇÃO | O usuário preferiu interromper\e[0m"
+    g_linha
+    exit
+  end
+end
+
+# Código Principal
+g_linha
+continuar = true # Condição para o while
+while continuar
+  num = input_float('Digite o número a ser arredondado: ')
+  num_a = arredondar(num)
+  mostrar_resultado(num, num_a)
+  # Encerrar o programa
+  continuar = encerramento
+end
+g_linha
