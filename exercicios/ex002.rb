d964acc9dd60672564186a7ad940bc379165ae63
NathaNz22 <128098319+NathaNz22@users.noreply.github.com>
2023-08-22 20:36:20 -0300

    Atualizado ex002

--- exercicios/ex002.rb
+++ exercicios/ex002.rb
@@ -1,9 +1,39 @@
 # Ler o nome de uma pessoa e mostrar uma mensagem de boas-vindas.
 
-puts ' Exercício 002 '.center(85, '=-')
+# Classe de boas vindas
+class BoasVindas
+  def initialize(nome)
+    @nome = nome
+  end
 
-print 'Qual o seu nome? : '
-nome = gets.chomp.strip.capitalize
-puts "Olá, #{nome}, seja bem-vindo(a)!"
+  def boas_vindas
+    puts "Olá, #{@nome}, seja bem-vindo(a)!"
+  end
+end
 
-puts ''.center(85, '=-')
+# Pede o nome do usuário
+def input_nome(msg)
+  loop do
+    print msg
+    nome = gets.chomp.strip.capitalize
+    return nome unless nome.empty?
+
+    puts "\e[31mERRO | Por favor, digite um nome válido\e[0m"
+  end
+end
+
+# Imprime uma linha personalizada
+def print_linha(width = 50)
+  # width é igual à tamanho da linha
+  puts ''.center(width, '=-')
+end
+
+# Programa Principal
+def main
+  print_linha
+  obj = BoasVindas.new(input_nome('Digite seu nome: '))
+  obj.boas_vindas
+  print_linha
+end
+
+main # Chama o programa principal
